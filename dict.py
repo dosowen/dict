9aec152b5da7264b5ff45a897400306d812f7318
dosowen <s103394@cmes.tn.edu.tw>
2018-07-20 21:15:02 +0800

    first

---
+++ dict.py
@@ -0,0 +1,29 @@
+words = {
+	'poo':'大便',
+	'tea':'茶'
+}
+snacks = {
+	'ice cream':'冰淇淋'
+}
+
+a = [words,snacks]
+print(a[0]['poo'])
+print(a[0]['tea'])
+print(a[1]['ice cream'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
